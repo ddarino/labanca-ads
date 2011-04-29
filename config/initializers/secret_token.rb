175d7d8bffea31c160e062122fe12047025fc042
salidux <dsalido@gmail.com>
2011-04-29 11:04:07 -0300

    subiendo...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LabancaAds::Application.config.secret_token = 'd206e45eb8ad7ca685aa8afff772796b283fa30cefff1d6721f411f0481aa881bdf16c6bff4eb147c2c2d532011a8c7d5e29ac5caa8eed2f487a33ebe2874e36'
