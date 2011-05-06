6d3a60d82806a47c7140a4d1d34fb8117da4d5ea
salidux <dsalido@gmail.com>
2011-05-06 11:56:31 -0300

    la cague en el migration, me olvide de guardargit add .!

--- db/migrate/20110506141916_add_price_to_advertisements.rb
+++ db/migrate/20110506141916_add_price_to_advertisements.rb
@@ -1,6 +1,6 @@
 class AddPriceToAdvertisements < ActiveRecord::Migration
   def self.up
-    add_column :advertisements, :price, :decimal
+    add_column :advertisements, :price, :decimal, :precision => 8, :scale => 2
   end
 
   def self.down
